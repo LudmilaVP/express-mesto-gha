07520319abf3684d082700d65b7800f59b55d57b
LudmilaVP <liudmila.lupandina@yandex.ru>
2022-12-01 14:18:15 +0300

    переделано форматирование

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const { handleError } = require('../utils/handleError');
-const { users } = require('./users');
-const { cards } = require('./cards');
+const router = require('express').Router();
+const usersRouter = require('./users');
+const cardsRouter = require('./cards');
+const { NOT_FOUND } = require('../utils/errors');
 
-const routes = express.Router();
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
 
-routes.use('/users', users);
-routes.use('/cards', cards);
-routes.all('*', (req, res) => {
-  const err = new Error('Неверный адрес запроса');
-  err.name = 'NotFoundError';
-  handleError(err, req, res);
+router.use((req, res) => {
+  res.status(NOT_FOUND).send('Сервер не найден');
 });
 
-module.exports = { routes };
+module.exports = router;
--- utils/errors.js
+++ utils/errors.js
@@ -1,26 +1,5 @@
-function handleError(err, req, res) {
-    console.log(err.name);
-  
-    if (err.name === 'CastError') {
-      res.status(400).send({
-        message: 'Неверный формат переданных данных',
-      });
-      return;
-    }
-    if (err.name === 'ValidationError') {
-        res.status(400).send({
-          message: err.message,
-        });
-        return;
-      }
-    if (err.name === 'NotFoundError') {
-      res.status(404).send({
-        message: err.message,
-      });
-      return;
-    }
-    res.status(500).send({
-      message: 'Не получилось обработать запрос',
-    });
-  }
-  module.exports = { handleError };
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+
+module.exports = { BAD_REQUEST, NOT_FOUND, SERVER_ERROR };
