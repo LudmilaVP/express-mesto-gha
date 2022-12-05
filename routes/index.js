88faafabaf1007cc3b280bc6a4efa9c0df0f2617
LudmilaVP <liudmila.lupandina@yandex.ru>
2022-12-05 09:12:01 +0300

    исправлено форматирование

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const { NOT_FOUND } = require('../utils/constants');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use('*', (req, res) => {
-    res.status(NOT_FOUND).send({ message: 'Ничего не найдено. Проверьте путь и метод запроса' });
-  });
+  res.status(NOT_FOUND).send({ message: 'Ничего не найдено. Проверьте путь и метод запроса' });
+});
 module.exports = router;
