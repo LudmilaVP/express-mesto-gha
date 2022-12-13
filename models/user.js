a7dd9fd5871bdcf39f84d67fc1709d6729b6008d
LudmilaVP <liudmila.lupandina@yandex.ru>
2022-12-13 15:14:19 +0300

    пр14 готовы модели

--- models/user.js
+++ models/user.js
@@ -1,26 +1,38 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isUrl = require('validator/lib/isURL');
+const isEmail = require('validator/lib/isEmail');
+const AuthError = require('../errors/AuthError.js');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь океана',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (url) => isUrl(url),
+      message: 'Неверный URL адрес',
+    },
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: (email) => isEmail(email),
+      message: 'Некорректый адрес почты',
+    },
   },
   password: {
     type: String,
@@ -29,5 +41,21 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: '',
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new AuthError('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new AuthError('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
