ed5e5a1a3ec5dd3bbbcda22684ca9643347a5afe
LudmilaVP <liudmila.lupandina@yandex.ru>
2022-12-20 09:44:28 +0300

    пр14 поправка пассворда

--- controllers/users.js
+++ controllers/users.js
@@ -43,21 +43,15 @@ const getUser = (req, res, next) => {
 
 const createUser = (req, res, next) => {
   const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
+    name, about, avatar, email, password,
   } = req.body;
 
   bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      email,
-      password: hash,
-      name,
-      about,
-      avatar,
-    }))
+    .then((hash) => User.create(
+      {
+        name, about, avatar, email, password: hash,
+      },
+    ))
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
