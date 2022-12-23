935b1f9387d0eeb2b5e0f45d796ca4fd5711fe74
LudmilaVP <liudmila.lupandina@yandex.ru>
2022-12-23 15:10:36 +0300

    add logs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,11 +16,13 @@ const {
 const { createUser, login } = require('./controllers/users');
 
 app.use(express.json());
+app.use(requestLogger);
 app.post('/signin', loginValidator, login);
 app.post('/signup', createUserValidator, createUser);
 
 app.use(auth);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
